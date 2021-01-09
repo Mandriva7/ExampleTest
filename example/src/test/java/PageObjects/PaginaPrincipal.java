817fe3b85798b7cdd98206d299e7b2b8a6acda81
Cristian <cristianbedoya1991@gmail.com>
2021-01-09 14:46:39 -0500

    More code....

--- example/src/test/java/PageObjects/PaginaPrincipal.java
+++ example/src/test/java/PageObjects/PaginaPrincipal.java
@@ -1,6 +1,20 @@
 package PageObjects;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class PaginaPrincipal {
 
+    @FindBy(css = "#twotabsearchtextbox")
+    private WebElement searchBox;
 
+    public void ingresarBusqueda() { searchBox.sendKeys("Software Test Design"); }
+    public void BuscarInfoIngresada() { searchBox.sendKeys(Keys.ENTER); }
+
+    public PaginaPrincipal(WebDriver driver){
+        PageFactory.initElements(driver,this);
+    }
 }
--- example/src/test/java/example/BaseTestSuite.java
+++ example/src/test/java/example/BaseTestSuite.java
@@ -1,6 +1,6 @@
 package example;
 
-
+import PageObjects.*;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
@@ -11,6 +11,13 @@ import java.util.concurrent.TimeUnit;
 public class BaseTestSuite {
 
     public WebDriver chromeDriver;
+
+    PaginaPrincipal paginaPrincipal;
+
+    private void inicializarPaginas(WebDriver driver){
+
+        paginaPrincipal = new PaginaPrincipal(driver);
+    }
 
 
     @Before
@@ -23,7 +30,9 @@ public class BaseTestSuite {
         //definimos tiempo de espera hasta que aceptemos timeouts
         chromeDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-
+        inicializarPaginas(chromeDriver);
+        //Abrimos una URL
+        chromeDriver.get("https://www.amazon.com/");
     }
     @After
     public void cerrarDriver()
